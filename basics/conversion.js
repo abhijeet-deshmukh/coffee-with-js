60ec202e1a4969d7a139e453a8913539ce1581d1
abhijeet-deshmukh <abhijeet.deshmukh084@gmail.com>
2024-02-19 20:13:46 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,20 @@
+
+let score ="33";
+console.log(score);
+console.log(typeof score);
+
+let valueInnumber = Number(score);
+console.log(typeof valueInnumber)
+
+//let score="33abc" it will show number when we see typeof this string but if  we see the value it will show NaN
+//when let score=null it will show 0
+//in case of undefined it will give you NaN
+let isloggedIn=1;
+let booleanIsLoggedIn=Boolean(isloggedIn);
+console.log(booleanIsLoggedIn); //outcome ==> true
+//empty string ==> output false
+
+let somenumber =33
+let stringNumber=String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
