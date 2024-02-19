cff60cf774b62a441fc3324392c88e358bc2adb2
abhijeet-deshmukh <abhijeet.deshmukh084@gmail.com>
2024-02-19 19:55:07 +0000

    basics

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat  all js code as newer version
+//alert(3+6); //though we are using node js so it will throw and says alert is not defined while it will work in browser.
+//Install Prettier extension for code radibility
+
+
+let a="abhijeet"
+let b=9;
+let c=true;
+//number
+//bigint
+//string
+//boolean
+//null ==> standalone value
+//undefined
+//symbol==> when we want to talk uniqueness then we use symbol
+//object
+console.log(typeof null) //it will show object
+console.log(typeof undefined); //it will show undefined as it is a type 
--- basics/variable.js
+++ basics/variable.js
@@ -1,11 +1,13 @@
-const accountId= 1234
- let accountEmail="abhijeet@gmail.com"
+const accountId= 1234 //the value cannot be changed
+let accountEmail="abhijeet@gmail.com"
  var accountPassword="12345"
  accountCity= "Kolkata"
-let accountState;
+let accountState;  // If the variable does not have value it would be considerd as undefined.
 //this is not allowed as const value cannot be changed
 //accountId=23
 
-console.log(accountId)
+console.log(accountId) //log likh ke entermaro console.log will appear
+
 //In modern javascript always use "let" as there was issue in use of var for its scope problem
+
 console.table([accountId,accountEmail,accountEmail,accountPassword,accountState,accountCity])
